0e3f0818f07b2032b8fc6c0d93e014b20c8bb602
rishabh-live <rishabh12536@gmail.com>
2021-08-13 14:48:34 +0530

    Setting up Tomcat

---
+++ src/main/java/live/rishabh/learn/springboot/LessonApiApp.java
@@ -0,0 +1,14 @@
+package live.rishabh.learn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonApiApp.class, args);
+
+	}
+
+}
